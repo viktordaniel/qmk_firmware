87e5d526a4e1aad37b81d8751efcf118474d851d
viktor <viktor.daniel@retarus.de>
2020-01-08 21:28:39 +0100

    Fixed layout

--- keyboards/dz60/keymaps/viktord/keymap.c
+++ keyboards/dz60/keymaps/viktord/keymap.c
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT_directional(
       KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, RESET,
       ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, LCTL(LSFT(KC_PSCREEN)), KC_PSCREEN , KC_SYSREQ, KC_MS_WH_UP, ______,
-      ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, KC_MS_WH_DOWN, ______,
-      ______, KC_VOLU, KC_VOLD, KC_MUTE, KC_BRIU, KC_BRID , ______, ______, ______, ______, ______, ______, ______, KC_PGUP, KC_INSERT,
+      ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, ______, KC_MS_WH_DOWN, ______,
+      ______, KC_VOLD, KC_VOLU, KC_MUTE, KC_BRID, KC_BRIU , ______, ______, ______, ______, ______, ______, KC_PGUP, KC_INSERT,
       ______, ______, ______, ______, ______, ______, ______, ______, KC_HOME, KC_PGDOWN , KC_END
       ),
 };
