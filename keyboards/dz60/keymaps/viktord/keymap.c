f77ab1532c52ff4c6b3652d00bbebddd74ad00f6
Viktor Daniel <viktor.daniel@retarus.de>
2020-01-13 12:39:20 +0100

    Added alted  layer

--- keyboards/dz60/keymaps/viktord/keymap.c
+++ keyboards/dz60/keymaps/viktord/keymap.c
@@ -1,5 +1,10 @@
 #include QMK_KEYBOARD_H
 
+
+
+#define _QWERTY 0
+#define _ALT 1
+#define _FUNC 2
 #define ______ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -14,16 +19,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl |  Cmd  |  Alt  |              Space                | FN | RAlt  |  L  |  D  |  R  |
+   * | Ctrl |  Cmd  |  Alt(1)  |              Space                | FN(2) | RAlt  |  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
 
-  LAYOUT_directional(
+  [_QWERTY] = LAYOUT_directional(
       KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      LT(1,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      LT(2,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, MO(1),KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL, KC_LGUI, LM(1,MOD_LALT), KC_SPC, KC_SPC, KC_SPC, MO(2),KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+      ),
+  /* Alted
+   * ,-----------------------------------------------------------------------------------------.
+   * | ` ~ |     |     |     |     |     |     |     |     |     |     |     |     |           |
+   * |-----------------------------------------------------------------------------------------+
+   * |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
+   * |-----------------------------------------------------------------------------------------+
+   * |         |     |     |     |     |     |     |     |     |     |     |     |             |
+   * |-----------------------------------------------------------------------------------------+
+   * |           |     |     |     |     |     |     |     |     |     |     |     |     |     |
+   * |-----------------------------------------------------------------------------------------+
+   * |      |       |       |                                   |    |       |     |     |     |
+   * `-----------------------------------------------------------------------------------------'
+   */
+
+  [_ALT] = LAYOUT_directional(
+      KC_GRV, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+      ______,______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+      ______, ______, ______, ______, ______, ______, ______,______, ______, ______, ______
       ),
 
   /* FN Layer
@@ -40,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------------'
    */
 
-  LAYOUT_directional(
+  [_FUNC] = LAYOUT_directional(
       KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, RESET,
-      ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, LCTL(LSFT(KC_PSCREEN)), KC_PSCREEN , KC_SYSREQ, KC_MS_WH_UP, ______,
+     ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, LCTL(LSFT(KC_PSCREEN)), KC_PSCREEN , KC_SYSREQ, KC_MS_WH_UP, ______,
       ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, ______, ______, ______, ______, ______, ______, KC_MS_WH_DOWN, ______,
       ______, KC_VOLD, KC_VOLU, KC_MUTE, KC_BRID, KC_BRIU , ______, ______, ______, ______, ______, ______, KC_PGUP, KC_INSERT,
       ______, ______, ______, ______, ______, ______, ______, ______, KC_HOME, KC_PGDOWN , KC_END
