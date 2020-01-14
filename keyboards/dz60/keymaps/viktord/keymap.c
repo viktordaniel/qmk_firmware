cc92b5ccbf8b44609446c4724ff4f685e5af7fdd
Viktor Daniel <viktor.daniel@retarus.de>
2020-01-14 08:48:22 +0100

    Swapped Ralt and FN, added mouse middle click

--- keyboards/dz60/keymaps/viktord/keymap.c
+++ keyboards/dz60/keymaps/viktord/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
    * |-----------------------------------------------------------------------------------------+
-   * | Ctrl |  Cmd  |  Alt(1)  |              Space                | FN(2) | RAlt  |  L  |  D  |  R  |
+   * | Ctrl |  Cmd  |  Alt(1)  |              Space                | RAlt  | MO(2) |  L  |  D  |  R  |
    * `-----------------------------------------------------------------------------------------'
    */
 
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       LT(2,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT,         KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-      KC_LCTL, KC_LGUI, LM(1,MOD_LALT), KC_SPC, KC_SPC, KC_SPC, MO(2),KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL, KC_LGUI, LM(1,MOD_LALT), KC_SPC, KC_SPC, KC_SPC, KC_RALT, MO(2), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
   /* Alted
    * ,-----------------------------------------------------------------------------------------.
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * |         | BL T| BL M| BL+ | BL- |     |     |     |     |     |     | WhDN|             |
    * |-----------------------------------------------------------------------------------------+
-   * |           | Vol+| Vol-| Mute| Bri+| Bri-|     |     |     |     |     |     | PUP | Ins |
+   * |           | Vol+| Vol-| Mute| Bri+| Bri-|     |     |     |     |M_BTN3|    | PUP | Ins |
    * |-----------------------------------------------------------------------------------------+
    * |      |       |       |                                   |     |      |Home | PDN | End |
    * `-----------------------------------------------------------------------------------------'
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS, RESET,
      ______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, LCTL(LSFT(KC_PSCREEN)), KC_PSCREEN , KC_SYSREQ, KC_MS_WH_UP, ______,
       ______, BL_TOGG, BL_STEP, BL_INC, BL_DEC, BL_BRTG, ______, ______, ______, ______, ______, KC_MS_WH_DOWN, ______,
-      ______, KC_VOLD, KC_VOLU, KC_MUTE, KC_BRID, KC_BRIU , ______, ______, ______, ______, ______, ______, KC_PGUP, KC_INSERT,
+      ______, KC_VOLD, KC_VOLU, KC_MUTE, KC_BRID, KC_BRIU , ______, ______, ______, ______, KC_BTN3, ______, KC_PGUP, KC_INSERT,
       ______, ______, ______, ______, ______, ______, ______, ______, KC_HOME, KC_PGDOWN , KC_END
       ),
 };
